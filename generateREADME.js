ffe19b8ab9f9f3f7ccf6db978e259ec1e84c8274
black-dev-x <thiago.dev.x@gmail.com>
2024-06-22 13:19:26 -0300

    update

--- generateREADME.js
+++ generateREADME.js
@@ -99,10 +99,10 @@ makeTable()
 
 
 h3("Others")
+g("Kafka", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTVa3Oi4m4gM8IrnBU-Ct0PwsXgCRe6cfLMhiLytNWjiL6B918z7OHnGrN3nshGU18WV3s&usqp=CAU")
 g("Raylib", "https://upload.wikimedia.org/wikipedia/commons/f/f4/Raylib_logo.png")
 g("Biopython", "https://biopython.org/assets/images/biopython_logo_white.png")
 g("Streamlit", "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/streamlit/streamlit-original.svg")
-g("Kafka", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTVa3Oi4m4gM8IrnBU-Ct0PwsXgCRe6cfLMhiLytNWjiL6B918z7OHnGrN3nshGU18WV3s&usqp=CAU")
 makeTable()
 
 h3("Some Links")
